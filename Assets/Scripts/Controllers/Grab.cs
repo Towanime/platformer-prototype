a5d52e553956ea06fb31296588fc86fd540a7d2f
Viktor Briceno <viktor.briceno@gmail.com>
2017-01-26 16:33:03 -0500

    Initial push + movement.

---
+++ Assets/Scripts/Controllers/Grab.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+    public float grabDistance = 3;
+    public float throwSpeed = 5;
+    // arm needs a rigid body and colission
+    public GameObject arm;
+    public bool isEnabled = true;
+    // 
+    private bool checkDistance;
+    private float initialPositionX;
+    private float finalPositionX;
+    private int throwDirection;
+    private SimplePlayerController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<SimplePlayerController>();
+	}
+	
+	void FixedUpdate () {
+        if (!isEnabled || !checkDistance) return;
+
+        // check distance manually 
+        if (checkDistance && arm.transform.position.x >= finalPositionX)
+        {
+            // return with penalty
+            Rigidbody2D rb = arm.GetComponent<Rigidbody2D>();
+
+            rb.velocity = new Vector2((throwDirection * -1) * throwSpeed, rb.velocity.y);
+            arm.GetComponent<BoxCollider2D>().enabled = false;
+        }
+	}
+
+    public void Begin(int direction)
+    {
+        checkDistance = true;
+        throwDirection = direction;
+        initialPositionX = arm.transform.position.x;
+        finalPositionX = initialPositionX + grabDistance;
+        Rigidbody2D rb = arm.GetComponent<Rigidbody2D>();
+        arm.GetComponent<BoxCollider2D>().enabled = true;
+
+        rb.velocity = new Vector2(direction * throwSpeed, rb.velocity.y);
+    }
+
+    public bool IsEnabled
+    {
+        set
+        {
+            this.isEnabled = value;
+        }
+        get
+        {
+            return this.isEnabled;
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/SimplePlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayerController : MonoBehaviour {
+    public float speed;             //Floating point variable to store the player's movement speed.
+    public float maxSpeed;
+    private bool facingRight = true;
+    private Rigidbody2D rigidbody;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public bool isEnabled = true;
+    // skills
+    private Grab grabSkill;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rigidbody = GetComponent<Rigidbody2D>();
+        grabSkill = GetComponent<Grab>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        if (!isEnabled) return;
+
+        // grab?
+        bool grab = Input.GetButton("Fire1");
+        if (grab)
+        {
+            // initiate grab and disable controller until is done
+            grabSkill.Begin(facingRight?1:-1);
+            IsEnabled = false;
+        }
+
+        float move = Input.GetAxis("Horizontal");
+
+        rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        } else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    public bool IsEnabled
+    {
+        set
+        {
+            this.isEnabled = value;
+        }
+        get
+        {
+            return this.isEnabled;
+        }
+    }
+}
