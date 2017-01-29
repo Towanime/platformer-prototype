bed064c65967b27c1c3e2c6046391f8572c5088c
Viktor Briceno <viktor.briceno@gmail.com>
2017-01-28 23:08:47 -0500

    Grab mechanic + crush implemented.

---
+++ Assets/Scripts/Mechanics/GrababbleEntity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrababbleEntity : MonoBehaviour {
+
+    /// <summary>
+    /// Checks conditions and prepares a body to be able attached to the arm.
+    /// </summary>
+    /// <returns>Returns true if the grab is successful or false if there's a condition to meet before grabbing an enemy.</returns>
+	public virtual bool OnGrab()
+    {
+        return true;
+    }
+}
