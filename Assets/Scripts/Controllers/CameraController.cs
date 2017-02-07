1d7a61594a282c0332dac71a6d59d4d8f1cf8a15
luisboscan <boscanp.luis@gmail.com>
2017-02-06 21:39:03 -0500

    Improved balance on dark lord values

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 distance;
+
+	// Use this for initialization
+	void Start () {
+        distance = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position - distance;
+	}
+}
